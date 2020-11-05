ceed946fa4c2ee4a0a3dd2f824cb40b1c41b85e4
mguti19 <mguti19@bancodebogota.com.co>
2020-11-05 18:07:44 -0500

    Skeleton done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './component/users/users.component';
+import { RegisterComponent } from './components/users/register/register.component';
+import { LoginComponent } from './components/users/login/login.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    RegisterComponent,
+    LoginComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
