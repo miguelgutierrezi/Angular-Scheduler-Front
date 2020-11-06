380b548f20040188116b4b1643746ba9b05ca721
mguti19 <mguti19@bancodebogota.com.co>
2020-11-06 17:18:46 -0500

    Changed back endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backUrl: 'http://localhost:3000/api/v1',
+  backUrl: 'https://nodejs-scheduler-back.herokuapp.com/api/v1',
   cypherKey: 'SCHEDULER_CYPHER_KEY'
 };
