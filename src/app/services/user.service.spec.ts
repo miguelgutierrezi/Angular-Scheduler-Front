14c845faff37780452365406323ad5a2ca64f1b3
mguti19 <mguti19@bancodebogota.com.co>
2020-11-06 15:49:29 -0500

    Complete unit tests for User service

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,16 +1,79 @@
-import { TestBed } from '@angular/core/testing';
+import {inject, TestBed} from '@angular/core/testing';
 
 import { UserService } from './user.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {User} from '../models/user';
 
 describe('UserService', () => {
-  let service: UserService;
+  const user: User = new User();
+  user.id = 'Id';
+  user.name = 'Name';
+  user.username = 'Username';
+  user.password = 'Password';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService]
+    });
   });
 
-  it('should be created', () => {
+  it('service should be created', inject([UserService], (service: UserService) => {
     expect(service).toBeTruthy();
-  });
+  }));
+
+  it('service should send login request', inject([UserService], (service: UserService) => {
+    service.login(user.username, user.password);
+    expect(service).toBeTruthy();
+  }));
+
+  it('service should send login request and throw error', inject([UserService], (service: UserService) => {
+    expect(() => service.login(undefined, undefined)).toThrowError();
+    expect(service).toBeTruthy();
+  }));
+
+  it('service should send getUsers request', inject([UserService], (service: UserService) => {
+    service.getUsers();
+    expect(service).toBeTruthy();
+  }));
+
+  it('service should send getUser request', inject([UserService], (service: UserService) => {
+    service.getUser(user.id);
+    expect(service).toBeTruthy();
+  }));
+
+  it('service should send getUser request and throw error', inject([UserService], (service: UserService) => {
+    expect(() => service.getUser(undefined)).toThrowError();
+    expect(service).toBeTruthy();
+  }));
+
+  it('service should send createUser request', inject([UserService], (service: UserService) => {
+    service.createUser(user);
+    expect(service).toBeTruthy();
+  }));
+
+  it('service should send createUser request and throw error', inject([UserService], (service: UserService) => {
+    expect(() => service.createUser(undefined)).toThrowError();
+    expect(service).toBeTruthy();
+  }));
+
+  it('service should send updateUser request', inject([UserService], (service: UserService) => {
+    service.updateUser(user.id, user);
+    expect(service).toBeTruthy();
+  }));
+
+  it('service should send updateUser request and throw error', inject([UserService], (service: UserService) => {
+    expect(() => service.updateUser(undefined, undefined)).toThrowError();
+    expect(service).toBeTruthy();
+  }));
+
+  it('service should send deleteUser request', inject([UserService], (service: UserService) => {
+    service.deleteUser(user.id);
+    expect(service).toBeTruthy();
+  }));
+
+  it('service should send deleteUser request and throw error', inject([UserService], (service: UserService) => {
+    expect(() => service.deleteUser(undefined)).toThrowError();
+    expect(service).toBeTruthy();
+  }));
 });
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {Observable} from 'rxjs';
+import {User} from '../models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -14,23 +15,56 @@ export class UserService {
     private http: HttpClient
   ) { }
 
+  public login(username: string, password: string): Observable<any> {
+    if (!username || !password) {
+      throw new Error('missing params');
+    }
+    const body = {
+      username,
+      password
+    };
+    return this.http.post(`${UserService.BASE_URL}/users`, body);
+  }
+
   public getUsers(): Observable<any> {
     return this.http.get(`${UserService.BASE_URL}/users`);
   }
 
   public getUser(id: string): Observable<any> {
+    if (!id) {
+      throw new Error('id is required');
+    }
     return this.http.get(`${UserService.BASE_URL}/users/${id}`);
   }
 
-  public createUser(): Observable<any> {
-    return this.http.post(`${UserService.BASE_URL}/users`, {});
+  public createUser(user: User): Observable<any> {
+    if (!user) {
+      throw new Error('user is required');
+    }
+    const body = {
+      name: user.name,
+      username: user.username,
+      password: user.password
+    };
+    return this.http.post(`${UserService.BASE_URL}/users`, body);
   }
 
-  public updateUser(id: string): Observable<any> {
-    return this.http.put(`${UserService.BASE_URL}/users/${id}`, {});
+  public updateUser(id: string, user: User): Observable<any> {
+    if (!id || !user) {
+      throw new Error('missing arguments');
+    }
+    const body = {
+      name: user.name,
+      username: user.username,
+      password: user.password
+    };
+    return this.http.put(`${UserService.BASE_URL}/users/${id}`, body);
   }
 
   public deleteUser(id: string): Observable<any> {
+    if (!id) {
+      throw new Error('id is required');
+    }
     return this.http.delete(`${UserService.BASE_URL}/users/${id}`);
   }
 }
